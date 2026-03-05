02decc0ab42e1d48795c7325c8c8003b9c9d5a68
NexaFlowDigital <erika@nexaflowdigital.com>
2026-03-05 13:14:48 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.PHOTOBOOTH_CONFIG = {
-  GAS_POST_URL: "https://script.google.com/macros/s/AKfycbzUVMniRgk9CdqPpF4x223YSiikm2Clmi8OYfgsNlW8OVmNKQ0LzDHYHLC5NDoAtiCI/exec"
+  GAS_POST_URL: "https://script.google.com/macros/s/AKfycbxcKS53rVGR8vU8FDyudj9h2u437huHfzqQlc5ijjf5ZEFaRQV7dFNVwgzFJz52vtk7/exec"
 };
